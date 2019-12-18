82ea5810eb096857d7301a4d373301d2f8aeba25
Kim-DongGeun <gther2486@naver.com>
2019-12-18 18:08:01 +0900

    complete

--- face_recognition.py
+++ face_recognition.py
@@ -5,6 +5,7 @@ import boto3
 import json
 from collections import OrderedDict
 from datetime import datetime
+from time import sleep
 
 data = dict()
 group_data = dict()
@@ -51,54 +52,65 @@ cam.set(4, 480) # set video height
 # Define min window size to be recognized as a face
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
-with open('log.json', 'w+') as make_file:
-    while True:
-        ret, img =cam.read()
-        #img = cv2.flip(img, -1) # Flip vertically
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        
-        faces = faceCascade.detectMultiScale( 
-            gray,
-            scaleFactor = 1.2,
-            minNeighbors = 5,
-            minSize = (int(minW), int(minH)),
-        )
 
-        for(x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-            # Check if confidence is less them 100 ==> "0" is perfect match
-            if (confidence < 100):
-                #data = json.loads(s)   
-                if control[id] == 0:
-                    control[id] = 1
-                    data['name'] = names[id]
-                    data['in_time'] = '%s' % (datetime.now())
-                    data['success'] = 'Y'
-                    group_data.append(data)
+while True:
+    ret, img =cam.read()
+    #img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+    )
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100):
+            #data = json.loads(s)   
+            if control[id] == 0:
+                control[id] = 1
+                data['name'] = names[id]
+                data['in_time'] = '%s' % (datetime.now())
+                data['success'] = 'Y'
+                group_data.append(data)
+                with open('log.json', 'w+') as make_file:
                     json.dump(group_data, make_file, ensure_ascii=False, indent='\t')
-                id = names[id]
-                confidence = "  {0}%".format(round(100 - confidence))
-            else:
-                if unknown == 0:
-                    unknown = 1
-                    data['name'] = 'unknown'
-                    data['in_time'] = '%s' % (datetime.now())
-                    data['success'] = 'N'
-                    group_data.append(data)
+                if round(100 - confidence) >= 50:
+                    s3.upload_file('log.json', bucket_name, 'log.json', ExtraArgs={'ACL':'public-read'})
+            id = names[id]
+            #confidence = "  {0}%".format(round(100 - confidence))
+            confidence = round(100 - confidence)
+        else:
+            if unknown == 0:
+                unknown = 1
+                data['name'] = 'unknown'
+                data['in_time'] = '%s' % (datetime.now())
+                data['success'] = 'N'
+                group_data.append(data)
+                with open('log.json', 'w+') as make_file:
                     json.dump(group_data, make_file, ensure_ascii=False, indent='\t')
-                id = "unknown"
-                confidence = "  {0}%".format(round(100 - confidence))
-            
+                s3.upload_file('log.json', bucket_name, 'log.json', ExtraArgs={'ACL':'public-read'})
+            id = "unknown"
+            #confidence = "  {0}%".format(round(100 - confidence))
+            confidence = round(100 - confidence)
+        
+        if str(id) != 'unknown' and int(confidence) >= 50:
             cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
             cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-        
-        cv2.imshow('camera',img) 
-        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
-        if k == 27:
-            break
+        elif id == 'unknown':
+            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
-s3.upload_file('log.json', bucket_name, 'log.json', ExtraArgs={'ACL':'public-read'})
+#s3.upload_file('log.json', bucket_name, 'log.json', ExtraArgs={'ACL':'public-read'})
 cam.release()
 cv2.destroyAllWindows()
